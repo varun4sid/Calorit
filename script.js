104c76c8a264fbc97f0537c3a7384b2f6fe6311f
varun <varun4socials@gmail.com>
2025-01-25 08:55:42 +0530

    Feature add entry

---
+++ script.js
@@ -0,0 +1,16 @@
+const addEntryButton = document.getElementById('add-entry');
+const entryDropdown = document.getElementById('entry-dropdown');
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories"
+    />`;
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+}
+
+addEntryButton.addEventListener("click", addEntry);
